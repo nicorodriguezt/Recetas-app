c3b0f5587a34cdd48a39786cdbba09f29885a9be
nicorodriguezt <nicorodriguezt@gmail.com>
2018-10-11 15:27:01 -0300

    aaaaaa

--- BackRecetas/database/Mongoose Connection.js
+++ BackRecetas/database/Mongoose Connection.js
@@ -1,5 +1,5 @@
 'use strict';
 var mongoose = require('mongoose');
-var URL = "mongodb://nicorodriguezt:nicolas_2995@ds129393.mlab.com:29393/recetas-app";
+var URL = "mongodb://nicolasrt:nico1234@ds129393.mlab.com:29393/recetas-app";
 mongoose.connect(URL);
 module.exports = mongoose;
