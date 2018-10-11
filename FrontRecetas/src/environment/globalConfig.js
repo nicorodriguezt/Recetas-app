16099688b2ae23c2cc7a59d4ad4c27c070472cb5
nicorodriguezt <nicorodriguezt@gmail.com>
2018-10-11 12:25:19 -0300

    prueba

--- FrontRecetas/src/environment/globalConfig.js
+++ FrontRecetas/src/environment/globalConfig.js
@@ -1,1 +1,22 @@
-export const apiURL = 'http//localhost:3000/'
+export const ENDPOINTS = {
+    development: {
+        product: 'http://localhost:3000/api'
+    },
+    staging: {
+        product: 'http://localhost:3000/api'
+    },
+    production: {
+        product: '/api'
+    },
+    getURL( service )  {
+        let env = process.env.NODE_ENV;
+        let environment= this;
+        console.log('Environment:' +env);
+        console.log('Servicio:' +service);
+        if( !environment[env] )
+            throw 'Environment not found';
+
+        console.log(`CurrentEndpoint: ${environment[env].product}`)
+        return environment[env][service];
+    }
+}
