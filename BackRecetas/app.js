0e267838a7fb5ad616f3302d4cdf07d40a76bb6e
Leandro Coggiola <leandrocoggiola@hotmail.com>
2018-10-11 14:55:53 -0300

    Cambios

--- BackRecetas/app.js
+++ BackRecetas/app.js
@@ -45,7 +45,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/index.js')));
+app.use(express.static(path.join(__dirname + '/client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors(corsOptions));
